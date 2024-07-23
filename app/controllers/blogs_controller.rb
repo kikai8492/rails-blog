27be2d1b6f7abb8a2a638b108a8401db7a2f7a93
shuto kikai <yysskikai04@icloud.com>
2024-07-23 10:38:03 +0900

    commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,29 @@
+class BlogsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+end
